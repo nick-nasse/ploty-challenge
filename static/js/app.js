47d8d023778eec4d2d88dc43e7c0cb82773e11c7
Nick Nasse <nasse@Nasses-Pro-Book.local>
2020-09-30 14:00:35 -0700

    functions for bar graph and change events

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,47 @@
 
-// use d3 to read json into variable
-d3.json("samples.json").then((sdata) => { 
-    console.log(sdata)
-});
+// function for creating bar graph 
+function plotting(subject) {
 
+    // use d3 to read json into variable
+    d3.json("samples.json").then((sdata) => { 
+        console.log(sdata)
+
+        // create variables to store values from sample data
+        var samples = sdata.samples.filter(a => a.id.toString() === subject)[0];
+
+        var samplevalues = samples.sample_values.slice(0, 10).reverse();
+
+        var top_otu = (samples.otu_ids.slice(0, 10)).reverse();
+
+        var id_otu = top_otu.map(b => "OTU " + b)
+
+        var labels = samples.otu_labels.slice(0, 10);
+
+        // Plot Bar Graph
+        var trace1 = {
+            x: samplevalues,
+            y: id_otu,
+            text: labels,
+            type: "bar",
+            orientation: "h",
+        };
+
+        var bar_data = [trace1];
+
+        var bar_layout = {
+            title: "OTU - Top 10",
+        };
+
+        Plotly.newPlot("bar", bar_data, bar_layout);
+
+    });
+
+};
+
+// change event function
+function optionChanged(subject) {
+    plotting(subject);
+}
 
 // function for taking sampledata and populating dropdown
 function init() {
@@ -14,7 +52,9 @@ function init() {
 
         sdata.names.forEach(function (name) {
             dropdown.append("option").text(name).property("value");
-        })
+        });
+
+        plotting(sdata.names[0]);
     });
 }
 
